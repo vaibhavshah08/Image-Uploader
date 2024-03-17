5cc459073e71898943d5610560b4afd9f1276969
vaibhavshah08 <shah_vaibhav@srmap.edu.in>
2024-03-17 22:39:46 +0530

    increase expiry time

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
-    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    const expiryDate = new Date(Date.now() +  86400000); // 1 day
     res
       .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
       .status(200)
